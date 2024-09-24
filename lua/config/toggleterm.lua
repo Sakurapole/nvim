621432328d15d7b203c53f658d01819bcae6b811
Sakurapole <1831495788@qq.com>
2024-09-25 00:48:54 +0800

    now term can show or hide by fastkey

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,8 +1,15 @@
 --- @type ToggleTermConfig
 require('toggleterm').setup {
   shell = 'pwsh.exe',
+  insert_mappings = true,
+  terminal_mappings = true,
   direction = 'float',
   persist_size = true,
   persist_mode = true,
-
+  autochdir = true,
+  shade_terminals = true,
+  float_opts = {
+    border = 'curved',
+    title_pos = 'center',
+  },
 }
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,8 +3,8 @@ vim.g.maplocalleader = " "
 
 -- define common options
 local opts = {
-	noremap = true, -- non-recursive
-	silent = true, -- do not show message
+  noremap = true, -- non-recursive
+  silent = true,  -- do not show message
 }
 local map = vim.api.nvim_set_keymap
 -----------------
@@ -77,6 +77,23 @@ vim.keymap.set("n", "<leader>ff", tb.find_files, { desc = "Telescope find files"
 vim.keymap.set("n", "<leader>fg", tb.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", tb.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", tb.help_tags, { desc = "Telescope help tags" })
+
+-- ToggleTerm
+vim.keymap.set("n", "<A-`>", ":ToggleTerm<CR>", opts)
+
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+function _G.set_terminal_keymaps()
+  local opts2 = { buffer = 0 }
+  vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]], opts2)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts2)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts2)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts2)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts2)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts2)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts2)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 -----------------
 -- Visual mode --
